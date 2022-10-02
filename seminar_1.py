d3e530d8489f9757e566eeac0c203efa0b013454
Salavat Faskhutdinov <salavat_faskhutdinov@MacBook-Air-Salavat.local>
2022-10-02 16:53:56 +0300

    add new changes

--- seminar_1.py
+++ seminar_1.py
@@ -12,13 +12,50 @@
 
 
 # Task 1
-a = int(input ("add new number: "))
-b = int(input ("add new number 2: "))
+# a = int(input ("add new number: "))
+# b = int(input ("add new number 2: "))
 
-if b == a * a : print (f'{a} * {a} = {b} -> True')
-elif a == b**2: print (f'{b} * {b} = {a} -> True')
-elif b != a**2: print (f'{a} * {a} != {b} -> FALSE')
-else : print (f'{b} * {b} != {a} -> FALSE')
+# if b == a * a : print (f'{a} * {a} = {b} -> True')
+# elif a == b**2: print (f'{b} * {b} = {a} -> True')
+# elif b != a**2: print (f'{a} * {a} != {b} -> FALSE')
+# else : print (f'{b} * {b} != {a} -> FALSE')
 
 
 #Task 2
+#напишите прогграмму , которая на вход принимает число N  и возвращает все числра от -N до N
+
+# a = int(input("Add new number: ")) 
+# b = -a
+
+# while b <= a : 
+#     print(b) 
+#     b+=1
+# else : print ("Stop")
+
+
+#Task 3
+
+#напишите программу которая будет принримать на вход дробное число и показывать первую цифру дробного числа.
+
+# a = float(input("Add new number: "))
+# b = round(a, 2)
+# c = int((b * 10) %10)
+# print(b)
+# print(c)
+
+numbers = list(map(int, input("Add new numbers: ").split()))#[1, 2, 3, 4, 5, 6]
+i = 0
+j = len(numbers)
+num = 0
+while i < j:
+    num = numbers[i] * numbers[j - 1]
+    print(num)
+    i+=1
+    j-=1
+else: print ("Stop")
+
+
+
+
+
+
